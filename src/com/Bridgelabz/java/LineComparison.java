f0c355ddd577168c09868c1b7bb5846b8b27d388
Kirandhanwai <kirandhanwai123@gmail.com>
2022-02-07 17:32:44 +0530

    Added : Welcome Line Comparison Computaion

---
+++ src/com/Bridgelabz/java/LineComparison.java
@@ -0,0 +1,9 @@
+package com.Bridgelabz.java;
+
+public class LineComparison {
+	public static void main (String[] args) {
+	
+		System.out.println("Welcome to Line Comparison Computation ");
+	}
+
+}
